cbf0691530df624f5dc18e4e83f148b08e43d7e2
Victor <yagovictorpaz@hotmail.com>
2025-08-19 22:14:46 -0300

    First Commit
    Manipulação de Picture Box e verificação de maior e menor idade

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblDataNascimento = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.dtpNascimento = new System.Windows.Forms.DateTimePicker();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.pibResultado = new System.Windows.Forms.PictureBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pibResultado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(329, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Secretaria do SENAC";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(302, 92);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(38, 13);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblDataNascimento
+            // 
+            this.lblDataNascimento.AutoSize = true;
+            this.lblDataNascimento.Location = new System.Drawing.Point(233, 118);
+            this.lblDataNascimento.Name = "lblDataNascimento";
+            this.lblDataNascimento.Size = new System.Drawing.Size(107, 13);
+            this.lblDataNascimento.TabIndex = 2;
+            this.lblDataNascimento.Text = "Data de Nascimento:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(346, 89);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(169, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // dtpNascimento
+            // 
+            this.dtpNascimento.Location = new System.Drawing.Point(346, 115);
+            this.dtpNascimento.MaxDate = new System.DateTime(2025, 8, 19, 0, 0, 0, 0);
+            this.dtpNascimento.MinDate = new System.DateTime(1900, 1, 1, 0, 0, 0, 0);
+            this.dtpNascimento.Name = "dtpNascimento";
+            this.dtpNascimento.Size = new System.Drawing.Size(169, 20);
+            this.dtpNascimento.TabIndex = 4;
+            this.dtpNascimento.Value = new System.DateTime(2010, 8, 19, 0, 0, 0, 0);
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Location = new System.Drawing.Point(304, 155);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(238, 29);
+            this.btnValidar.TabIndex = 5;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // pibResultado
+            // 
+            this.pibResultado.Location = new System.Drawing.Point(279, 208);
+            this.pibResultado.Name = "pibResultado";
+            this.pibResultado.Size = new System.Drawing.Size(287, 191);
+            this.pibResultado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pibResultado.TabIndex = 6;
+            this.pibResultado.TabStop = false;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(697, 307);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.pibResultado);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.dtpNascimento);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblDataNascimento);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Secretaria do Senac";
+            ((System.ComponentModel.ISupportInitialize)(this.pibResultado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblDataNascimento;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.DateTimePicker dtpNascimento;
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.PictureBox pibResultado;
+        private System.Windows.Forms.Label lblResultado;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,40 @@ namespace ExemploIdade
         {
             InitializeComponent();
         }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            // Verificar se o campo está vazio:
+            if (txtNome.Text == "")
+            {
+                MessageBox.Show("Digite o seu nome!", "Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else
+            {
+                // Pegar os Valores do Form:
+                string nome = txtNome.Text;
+                DateTime dataNascimento = dtpNascimento.Value;
+                
+                // Calcular a idade da pessoa:
+                int idade = DateTime.Now.Year - dataNascimento.Year;
+
+                // Verificar se o usuário tem menos de 16 anos:
+                if (idade < 16)
+                {
+                    pibResultado.Image = Properties.Resources.deslike_png;
+                    lblResultado.Text = $"{nome} tem {idade} anos\n não pode matricular no senac";
+                }
+                else
+                {
+                    // Mudar a imagem:
+                    pibResultado.Image = Properties.Resources.ok_png;
+                    lblResultado.Text = $"{nome} tem {idade} anos\n e já pode matricular no senac";
+                }
+            }
+        }
+
+
     }
 }
